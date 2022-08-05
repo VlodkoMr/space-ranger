d34db395bbb5a857fa1bca7240348cf7e13dc7c9
vlodkow <vlodkow@gmail.com>
2022-08-05 18:54:59 +0300

    Add assets

--- contracts/SpaceGame.sol
+++ contracts/SpaceGame.sol
@@ -1,19 +1,21 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.12;
 
-contract SpaceGame {
-  string public name = "My Hardhat Token";
-  string public symbol = "MHT";
-  uint256 public totalSupply = 1000000;
-  address public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  mapping(address => uint256) balances;
+contract SpaceGame is Ownable {
 
-//  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  uint shipsTypes = 0;
+  mapping(address => uint256) scores;
+  mapping(address => SpaceShip[]) scores;
+
+  struct SpaceShip{
+
+  }
+
+  //  event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
   constructor() {
-//    balances[msg.sender] = totalSupply;
-    owner = msg.sender;
   }
 
   function transfer(address to, uint256 amount) external {
