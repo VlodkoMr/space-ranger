b6b7b5e5a270de08fbb0c3f6756cf302fd154bfb
vlodkow <vlodkow@gmail.com>
2022-08-19 14:50:08 +0300

    Update ship - next level

--- abstract/Utils.sol
+++ abstract/Utils.sol
@@ -5,4 +5,27 @@ abstract contract Utils {
   function randomNumber(uint _max) internal view returns (uint) {
     return uint(keccak256(abi.encodePacked(uint(1), block.difficulty, block.timestamp, uint(1)))) % _max;
   }
+
+  function indexOf(uint[] memory self, uint value) internal pure returns (uint, bool) {
+    uint length = self.length;
+    for (uint i = 0; i < length; ++i) if (self[i] == value) return (i, true);
+    return (0, false);
+  }
+
+  function concatStrings(string memory a, string memory b) internal pure returns (string memory) {
+    return string(abi.encodePacked(a, b));
+  }
+
+  function stringToUint(string memory numString) public pure returns (uint) {
+    uint val = 0;
+    bytes   memory stringBytes = bytes(numString);
+    for (uint i = 0; i < stringBytes.length; i++) {
+      uint exp = stringBytes.length - i;
+      bytes1 iVal = stringBytes[i];
+      uint8 uVal = uint8(iVal);
+      uint jVal = uVal - uint(0x30);
+      val += (uint(jVal) * (10 ** (exp - 1)));
+    }
+    return val;
+  }
 }
--- contracts/SpaceRanger.sol
+++ contracts/SpaceRanger.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "../abstract/ShipHelper.sol";
+import "../abstract/Utils.sol";
 
-contract SpaceRanger is ERC1155, Ownable, ShipHelper {
+contract SpaceRanger is ERC1155, Ownable, ERC1155Burnable, ShipHelper, Utils {
   uint public constant SHIPS_TYPE_SUPPLY = 1000;
   uint16[] public mintedShips = [0, 0, 0, 0, 0];
   uint public totalMintedShips = 0;
 
-  mapping(address => uint) public userScores; // total scores
+  mapping(address => uint) public userBalance; // total scores balance
   mapping(address => uint) public userLevel; // last user level
   mapping(uint => Ship) public ships; // all spaceShips by id
   mapping(address => uint[]) public userShips; // list of user ships
@@ -40,7 +42,12 @@ contract SpaceRanger is ERC1155, Ownable, ShipHelper {
     ShipUpgradeType[] upgrades;
   }
 
-  constructor() ERC1155("https://bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia.ipfs.nftstorage.link/{id}.json") {}
+  constructor() ERC1155("https://bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia.ipfs.nftstorage.link/{id}.json") {
+    // Generate all 5000 spaceShips. Id of each ship type increase by 10 (11-51)
+    for (uint8 _i = 11; _i <= 51; _i += 10) {
+      _mint(address(this), _i, SHIPS_TYPE_SUPPLY, "");
+    }
+  }
 
   function uri(uint _tokenId) override public pure returns (string memory){
     return string(
@@ -56,6 +63,7 @@ contract SpaceRanger is ERC1155, Ownable, ShipHelper {
     _setURI(_uri);
   }
 
+  // Allow to mint 1 free ship per account
   function mint(uint8 _shipTypeId) public {
     require(_shipTypeId < mintedShips.length, "SpaceShip doesn't exists");
     require(_shipTypeId > 0, "Wrong SpaceShip ID");
@@ -65,7 +73,11 @@ contract SpaceRanger is ERC1155, Ownable, ShipHelper {
     require(mintedShips[_shipTypeIndex] < SHIPS_TYPE_SUPPLY, "No more ships of this modification in our station.");
 
     uint _id = ++totalMintedShips;
-    _mint(msg.sender, _id, 1, "");
+
+    // transfer NFT
+    string memory _shipTypeLevel = Utils.concatStrings(Strings.toString(_shipTypeId), Strings.toString(1));
+    safeTransferFrom(address(this), msg.sender, Utils.stringToUint(_shipTypeLevel), 1, "");
+    //    _mint(msg.sender, _id, 1, "");
     mintedShips[_shipTypeIndex] += 1;
 
     (uint8 _health, uint8 _attack, uint8 _speed,uint8 _weapons) = ShipHelper.getShipStats(_shipTypeId);
@@ -99,7 +111,7 @@ contract SpaceRanger is ERC1155, Ownable, ShipHelper {
   }
 
   // Upgrade SpaceShip characteristics
-  function upgradeShip(uint _id, ShipUpgradeType _upgradeType) public {
+  function upgradeShipCharacteristics(uint _id, ShipUpgradeType _upgradeType) public {
     Ship storage ship = ships[_id];
 
     require(ship.id == _id, "SpaceShip not found");
@@ -127,6 +139,46 @@ contract SpaceRanger is ERC1155, Ownable, ShipHelper {
     }
 
     ship.upgrades.push(_upgradeType);
+  }
+
+  function upgradeShipLevel(uint _id) public {
+    Ship storage ship = ships[_id];
+    (, bool _exist) = Utils.indexOf(userShips[msg.sender], _id);
+
+    require(_exist, "You don't have this SpaceShip");
+    require(ship.level < 3, "You can't increase SpaceShip level.");
+
+    uint8 _nextLevel;
+    if (ship.level == 1) {
+      require(userBalance[msg.sender] > 100000, "Need more balance");
+      userBalance[msg.sender] -= 100000;
+      _nextLevel = 2;
+    } else {
+      require(userBalance[msg.sender] > 500000, "Need more balance");
+      userBalance[msg.sender] -= 500000;
+      _nextLevel = 3;
+    }
+
+    // Burn old ship NFT
+    string memory _oldShipTypeLevel = Utils.concatStrings(Strings.toString(ship.shipType), Strings.toString(ship.level));
+    _burn(msg.sender, Utils.stringToUint(_oldShipTypeLevel), 1);
+
+    // Mint new NFT
+    string memory _newShipTypeLevel = Utils.concatStrings(Strings.toString(ship.shipType), Strings.toString(_nextLevel));
+    _mint(msg.sender, Utils.stringToUint(_newShipTypeLevel), 1, "");
+
+    // Update user ship data
+    ship.level = _nextLevel;
+
+    // TODO: Update by ship type
+    ship.health = 60;
+    ship.attack = 20;
+    ship.weapons = 2;
+    ship.maxEnergy = 20;
+    ship.speed = 6;
+
+    ship.onSale = false;
+    ship.salePrice = 0;
   }
 
   function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
