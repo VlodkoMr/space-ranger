75e358e61b8aa34318c2e8457b7ead0d54d8e742
vlodkow <vlodkow@gmail.com>
2022-08-19 12:55:30 +0300

    Update mint ship

--- contracts/SpaceRanger.sol
+++ contracts/SpaceRanger.sol
@@ -6,80 +6,83 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SpaceRanger is ERC1155, Ownable {
-	uint public constant SHIPS_TYPE_SUPPLY = 1000;
-	uint16[] public mintedShips = [0, 0, 0, 0, 0];
-	uint public totalMintedShips = 0;
+  uint public constant SHIPS_TYPE_SUPPLY = 1000;
+  uint16[] public mintedShips = [0, 0, 0, 0, 0];
+  uint public totalMintedShips = 0;
 
-	mapping(address => uint) public userScores; // total scores
-	mapping(address => uint) public userLevel; // last user level
-	mapping(address => Ship[]) public userShips;
+  mapping(address => uint) public userScores; // total scores
+  mapping(address => uint) public userLevel; // last user level
+  mapping(address => Ship[]) public userShips;
 
-	struct Ship {
-		uint id;
-		uint8 health;
-		uint8 attack;
-		uint8 weapons;
-		uint8 level;
-		uint8 shipType;
-		bool onSale;
-		uint salePrice;
-	}
+  struct Ship {
+    uint id;
+    uint8 health;
+    uint8 attack;
+    uint8 weapons;
+    uint8 level;
+    uint8 shipType;
+    bool onSale;
+    uint salePrice;
+  }
 
-	constructor() ERC1155("https://bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia.ipfs.nftstorage.link/{id}.json") {}
+  constructor() ERC1155("https://bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia.ipfs.nftstorage.link/{id}.json") {}
 
-	function uri(uint _tokenId) override public pure returns (string memory){
-		return string(
-			abi.encodePacked(
-				"https://ipfs.io/ipfs/bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia/",
-				Strings.toString(_tokenId),
-				".json"
-			)
-		);
-	}
+  function uri(uint _tokenId) override public pure returns (string memory){
+    return string(
+      abi.encodePacked(
+        "https://ipfs.io/ipfs/bafybeibxyhdne4x3uqblljkl2aetxvldtpb4lctstjqe22yr77gnrnlaia/",
+        Strings.toString(_tokenId),
+        ".json"
+      )
+    );
+  }
 
-	function setURI(string memory _uri) public onlyOwner {
-		_setURI(_uri);
-	}
+  function setURI(string memory _uri) public onlyOwner {
+    _setURI(_uri);
+  }
 
-	function mint(uint8 _shipTypeId) public {
-		require(_shipTypeId < mintedShips.length, "SpaceShip doesn't exists");
-		require(_shipTypeId > 0, "Wrong SpaceShip ID");
-		require(userShips[msg.sender].length == 0, "You already have SpaceShip");
+  function mint(uint8 _shipTypeId) public {
+    require(_shipTypeId < mintedShips.length, "SpaceShip doesn't exists");
+    require(_shipTypeId > 0, "Wrong SpaceShip ID");
+    require(userShips[msg.sender].length == 0, "You already have SpaceShip");
 
-		uint8 _shipTypeIndex = _shipTypeId - 1;
-		require(mintedShips[_shipTypeIndex] < SHIPS_TYPE_SUPPLY, "No more ships of this modification in our station.");
+    uint8 _shipTypeIndex = _shipTypeId - 1;
+    require(mintedShips[_shipTypeIndex] < SHIPS_TYPE_SUPPLY, "No more ships of this modification in our station.");
 
-		uint _id = ++totalMintedShips;
-		_mint(msg.sender, _id, 1, "");
-		mintedShips[_shipTypeIndex] += 1;
+    uint _id = ++totalMintedShips;
+    _mint(msg.sender, _id, 1, "");
+    mintedShips[_shipTypeIndex] += 1;
 
-		Ship memory _newShip = Ship({
-		id : _id,
-		health : 25,
-		attack : 10,
-		weapons : 1,
-		level : 1,
-		shipType : _shipTypeId,
-		onSale : false,
-		salePrice : 0
-		});
-		userShips[msg.sender].push(_newShip);
-	}
+    uint(_health, _attack, _speed, _weapons) = getShipStats(_shipTypeId);
 
-	function getUserShips(address _owner) public view returns (Ship[] memory){
-		Ship[] memory _result = new Ship[](userShips[_owner].length);
-		for (uint _i = 0; _i < userShips[_owner].length; ++_i) {
-			_result[_i] = userShips[_owner][_i];
-		}
-		return _result;
-	}
+    Ship memory _newShip = Ship({
+    id : _id,
+    health : _health,
+    attack : _attack,
+    weapons : _weapons,
+    speed : _speed,
+    level : 1,
+    shipType : _shipTypeId,
+    onSale : false,
+    salePrice : 0
+    });
+    userShips[msg.sender].push(_newShip);
+  }
 
-	function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-	internal
-	override(ERC1155)
-	{
-		super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-	}
+  function getUserShips(address _owner) public view returns (Ship[] memory){
+    Ship[] memory _result = new Ship[](userShips[_owner].length);
+    for (uint _i = 0; _i < userShips[_owner].length; ++_i) {
+      _result[_i] = userShips[_owner][_i];
+    }
+    return _result;
+  }
+
+  function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+  internal
+  override(ERC1155)
+  {
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+  }
 
 }
 
--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.9;
 
 abstract contract Utils {
-	function randomNumber(uint _max) internal view returns (uint) {
-		return uint(keccak256(abi.encodePacked(uint(1), block.difficulty, block.timestamp, uint(1)))) % _max;
-	}
+  function randomNumber(uint _max) internal view returns (uint) {
+    return uint(keccak256(abi.encodePacked(uint(1), block.difficulty, block.timestamp, uint(1)))) % _max;
+  }
+
+  function getShipStats(uint _id) internal view returns (uint, uint, uint, uint) {
+    if (_id == 11) {
+      return (25, 12, 5, 1);
+    } else if (_id == 21) {
+      return (26, 10, 6, 1);
+    } else if (_id == 31) {
+      return (21, 7, 4, 2);
+    } else if (_id == 41) {
+      return (26, 11, 5, 1);
+    } else {
+      return (23, 6, 4, 2);
+    }
+  }
 }
